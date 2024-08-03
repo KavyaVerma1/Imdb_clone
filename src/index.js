1e6b2474c4b1de41312218dd93fc1f385c15e921
KavyaVerma1 <132491171+KavyaVerma1@users.noreply.github.com>
2024-08-03 12:57:02 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+)
+
+// import React from 'react';
+// import { createRoot } from 'react-dom'; // Import createRoot from react-dom
+// import './index.css';
+// import App from './App';
+
+// // Use createRoot instead of ReactDOM.render
+// createRoot(document.getElementById('root')).render(<App />);
